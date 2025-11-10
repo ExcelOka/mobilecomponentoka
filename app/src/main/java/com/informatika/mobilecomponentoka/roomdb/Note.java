0405ab887377af57df1bbcfaa10aac3fce535600
Excel Oka Jonanta <jonaoka27@gmail.com>
2025-11-10 19:04:57 +0700

    first commit

---
+++ app/src/main/java/com/informatika/mobilecomponentoka/roomdb/Note.java
@@ -0,0 +1,12 @@
+package com.informatika.mobilecomponentoka.roomdb;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String text;
+}
+
